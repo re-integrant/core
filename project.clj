e0abe32d25cadf3a127df477bddb03791eda9414
Kazuki Tsutsumi <rixi223.kazuki@gmail.com>
2019-02-24 23:40:25 -0800

    Update description.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject re-integrant "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Modularized SPA library."
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
                  [org.clojure/clojurescript "1.10.520" :scope "provided"]
                  [re-frame "0.10.6"]
